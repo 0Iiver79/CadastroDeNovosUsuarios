bda861bc173fedeadfcd1889596935b49e772d0d
Oliver <gustavovizis@gmail.com>
2025-05-22 13:19:30 -0300

    Atualizações no Codigo

--- src/main/java/dev/java10x/CadastroDeUsuarios/Tarefas/TarefasModel.java
+++ src/main/java/dev/java10x/CadastroDeUsuarios/Tarefas/TarefasModel.java
@@ -1,4 +1,32 @@
 package dev.java10x.CadastroDeUsuarios.Tarefas;
 
+
+import dev.java10x.CadastroDeUsuarios.Usuarios.Controller.Service.UsuarioModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table (name = "tb_tarefas")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class TarefasModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  long id;
+
+    private String usuario;
+
+    private String tipo;
+
+    private String prazo;
+
+    // @OneToMany - Uma tarefa pode ter varios usuarios
+    @OneToMany(mappedBy = "tarefas" )
+    private List<TarefasModel> tarefas;
 }
--- src/main/java/dev/java10x/CadastroDeUsuarios/Usuarios/Controller/Service/UsuarioModel.java
+++ src/main/java/dev/java10x/CadastroDeUsuarios/Usuarios/Controller/Service/UsuarioModel.java
@@ -1,48 +1,36 @@
-package dev.java10x.CadastroDeUsuarios;
+package dev.java10x.CadastroDeUsuarios.Usuarios.Controller.Service;
 
+import dev.java10x.CadastroDeUsuarios.Tarefas.TarefasModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Table(name = "tb_cadastro_de_usuarios")
+import java.util.List;
+
+@Table(name = "tb_cadastro")
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 // @Entity tranforma uma class em uma entidade do DB(database)
 public class UsuarioModel {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String nome;
+
     private String email;
+
     private int idade;
 
-    public UsuarioModel() {
-    }
+    // @ManyToOne - Um usuario tem uma unica tarefa
+    @ManyToOne
+    @JoinColumn(name = "tarefas_id") // Foreina Key ou chave estrangeira
+    private TarefasModel tarefas;
 
-    public UsuarioModel(String email, String nome, int idade) {
-        this.email = email;
-        this.nome = nome;
-        this.idade = idade;
-    }
 
-    public String getNome() {
-        return nome;
-    }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
